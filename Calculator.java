fe786126a2c544858fd38f89c32b70669385b427
harshlsoni <168442458+harshlsoni@users.noreply.github.com>
2025-02-01 15:20:15 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,38 +1,43 @@
-// Calculator
-public class Calculator {
-	int sum() {
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sum = numbers[0] + numbers[1];
-		return sum;
-	}
-
-	int sub() {
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int sub = numbers[0] - numbers[1];
-		return sub;
-	}
-
-	int multi() {
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		int multi = numbers[0] * numbers[1];
-		return multi;
-	}
-
-	int div() {
-		UserInput ui = new UserInput();
-		int[] numbers = ui.UserInput();
-		if (numbers[1] == 0) {
-			return -1;
-		}
-		int div = numbers[0] / numbers[1];
-		return div;
-	}
-
-
-
-
-
+// Calculator.java
+class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+    public int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public double sumArray(double[] arr) {
+        double sum = 0;
+        for (double num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+    public double mean(double[] arr) {
+        return sumArray(arr) / arr.length;
+    }
+    public double variance(double[] arr) {
+        double mean = mean(arr);
+        double variance = 0;
+        for (double num : arr) {
+            variance += Math.pow(num - mean, 2);
+        }
+        return variance / arr.length;
+    }
+    public double standardDeviation(double[] arr) {
+        return Math.sqrt(variance(arr));
+    }
 }
