c2a44812ea303da343c03df6720e63dbf371ae5b
harshlsoni <168442458+harshlsoni@users.noreply.github.com>
2025-02-01 15:20:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,55 @@
-//main.java
-import java.util.Scanner;
-public class Main{
-	public static void main(String[] args) {
-		System.out.println("Name of operator: ");
-		Scanner scan = new Scanner(System.in);
-		String op = scan.nextLine();
-		Calculator calc = new Calculator();
-		if(op.equals("add")) {
-			System.out.println("The sum of two nums is : " + calc.sum());
-		}
-		else if(op.equals("sub")) {
-			System.out.println("The subtract of two nums is : " + calc.sub());
-		}
-		else if(op.equals( "multiply")) {
-			System.out.println("The product of two nums is : " + calc.multi());
-		}
-		else if (op.equals("divide"))  {
-			System.out.println("The division of two nums is : " + calc.div());
-		}
-		else {
-			System.out.println("Wrong operator...");
-		}
-	}
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        UserInput userInput = new UserInput();
+        Calculator calculator = new Calculator();
+        
+        int choice;
+        do {
+            choice = userInput.getMenuChoice();
+            switch (choice) {
+                case 1: // Addition
+                    double[] addNums = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.add(addNums[0], addNums[1]));
+                    break;
+                case 2: // Subtraction
+                    double[] subNums = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.subtract(subNums[0], subNums[1]));
+                    break;
+                case 3: // Multiplication
+                    double[] mulNums = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.multiply(mulNums[0], mulNums[1]));
+                    break;
+                case 4: // Division
+                    double[] divNums = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.divide(divNums[0], divNums[1]));
+                    break;
+                case 5: // Fibonacci
+                    int n = userInput.getSingleNumber();
+                    System.out.println("Fibonacci(" + n + ") = " + calculator.fibonacci(n));
+                    break;
+                case 6: // Sum of Array
+                    double[] arr = userInput.getArrayInput();
+                    System.out.println("Sum: " + calculator.sumArray(arr));
+                    break;
+                case 7: // Mean of Array
+                    arr = userInput.getArrayInput();
+                    System.out.println("Mean: " + calculator.mean(arr));
+                    break;
+                case 8: // Variance of Array
+                    arr = userInput.getArrayInput();
+                    System.out.println("Variance: " + calculator.variance(arr));
+                    break;
+                case 9: // Standard Deviation of Array
+                    arr = userInput.getArrayInput();
+                    System.out.println("Standard Deviation: " + calculator.standardDeviation(arr));
+                    break;
+                case 0: // Exit
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+            }
+        } while (choice != 0);
+    }
 }
